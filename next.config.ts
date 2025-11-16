d390517cf310fd6b356260916af093ff82002882
GaneshLindung <113347101+GaneshLindung@users.noreply.github.com>
2025-11-16 12:09:29 +0700

    Conditionally set allowedDevOrigins in config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    'localhost',
-    '127.0.0.1',
-    '192.168.1.11',
-    '172.30.235.127',
-  ],
-};
+const isDev = process.env.NODE_ENV === 'development';
+
+const nextConfig: NextConfig = isDev
+  ? {
+      allowedDevOrigins: [
+        'localhost',
+        '127.0.0.1',
+        '192.168.1.11',
+        '172.30.235.127',
+      ],
+    }
+  : {};
 
 export default nextConfig;
