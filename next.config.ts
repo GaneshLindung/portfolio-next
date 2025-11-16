d94fb6be3dd02858317f9987e5ac70fb6a2c8fcf
GaneshLindung <113347101+GaneshLindung@users.noreply.github.com>
2025-11-16 12:13:12 +0700

    Simplify next.config.ts by removing environment check

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from 'next';
 
-const isDev = process.env.NODE_ENV === 'development';
-
-const nextConfig: NextConfig = isDev
-  ? {
-      allowedDevOrigins: [
-        'localhost',
-        '127.0.0.1',
-        '192.168.1.11',
-        '172.30.235.127',
-      ],
-    }
-  : {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    '192.168.1.11',
+    '172.30.235.127',
+  ],
+};
 
 export default nextConfig;
